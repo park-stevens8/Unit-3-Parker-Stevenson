19e9a96723e74e82a56c748da8a18b75e022f4cb
Michael George <georgem@wlwv.k12.or.us>
2019-10-10 09:54:48 -0700

    palindrome and string

---
+++ PalindromeTester.java
@@ -0,0 +1,22 @@
+import java.util.Scanner;
+
+public class PalindromeTester{
+  public static void main(String []args){
+
+    Scanner input = new Scanner(System.in);
+
+    System.out.println("Would you like to enter a palindrome?");
+    //boolean answer
+    String palindrome = input.next();
+
+    //make whole thing lowercase
+    //get string length String(Length);
+    //make new string with reassigned order same length charAt
+
+    //compare strings to check for same unicode values
+
+
+
+
+  }
+}
---
+++ StringComparisons.java
@@ -0,0 +1,9 @@
+public class StringComparisons{
+  public static void main(String []args){
+
+    String word1 = "able";
+    String word2 = "Baker";
+
+    System.out.println(word1.compareTo(word2));
+  }
+}
